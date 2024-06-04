2200f86c871382e4eede538842285c4b65466ba3
ehall-e11 <ethan.hall@engineering11.com>
2024-06-04 11:51:14 -0500

    2.2-3

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
 alert(2)
-console.log(document.getElementsByTagName("a")[0])
+console.log(parent.document.getElementsByTagName("a")[0])
 document.getElementsByTagName("body")[0].innerHTML = "hmm"
 alert(3)

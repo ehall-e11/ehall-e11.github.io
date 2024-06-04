a871415b48d0fa66894e966f09808bbbeb9ba9c1
ehall-e11 <ethan.hall@engineering11.com>
2024-06-04 12:18:37 -0500

    2.2-9

--- script2.js
+++ script2.js
@@ -4,6 +4,6 @@ parent.document
   .getElementsByTagName("body")[0]
   .insertAdjacentHTML(
     "afterend",
-    "<img src='https://upload.wikimedia.org/wikipedia/commons/0/0c/Cow_female_black_white.jpg' alt = 'cow'></img>https://tenor.com/view/brian-family-guy-tiktok-funny-clip-tasty-sand-gif-27509497"
+    "<img src='https://upload.wikimedia.org/wikipedia/commons/0/0c/Cow_female_black_white.jpg' alt = 'cow'></img><div class='tenor-gif-embed' data-postid='27509497' data-share-method='host' data-aspect-ratio='1' data-width='100%'><a href='https://tenor.com/view/brian-family-guy-tiktok-funny-clip-tasty-sand-gif-27509497'>Brian Family Guy GIF</a>from <a href='https://tenor.com/search/brian-gifs'>Brian GIFs</a></div> <script type='text/javascript' async src='https://tenor.com/embed.js'></script>"
   )
 alert(3)

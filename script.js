577f7ba0fc6277a5056c504e8591932ed5732e81
ehall-e11 <ethan.hall@engineering11.com>
2024-06-04 10:46:07 -0500

    init

---
+++ script.js
@@ -0,0 +1,1 @@
+alert(`XSS`);const ele=parent.document.getElementsByTagName(`a`);for(let i=0;i<ele.length;i++){ele[i].href=`https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html`;

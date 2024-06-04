4670f2d92a300b8f7357b78bf21df4c630b87c08
ehall-e11 <ethan.hall@engineering11.com>
2024-06-04 11:59:05 -0500

    2.2-5

--- script2.js
+++ script2.js
@@ -1,4 +1,6 @@
 alert(2)
 console.log(parent.document.getElementsByTagName("a")[0])
-parent.document.getElementsByTagName("body")[0].innerHTML = "hmm"
+parent.document.getElementsByTagName("body")[0].innerHTML =
+  parent.document.getElementsByTagName("body")[0].innerHTML +
+  "<img src='https://upload.wikimedia.org/wikipedia/commons/0/0c/Cow_female_black_white.jpg' alt = 'cow'></img>"
 alert(3)

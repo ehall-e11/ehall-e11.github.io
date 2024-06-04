4a4a39d712750d70c740a65417411c774899b350
ehall-e11 <ethan.hall@engineering11.com>
2024-06-04 10:54:45 -0500

    formatting

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert(`XSS`);const ele=parent.document.getElementsByTagName(`a`);for(let i=0;i<ele.length;i++){ele[i].href=`https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html`;
+alert(`XSS`);
+const ele=parent.document.getElementsByTagName(`a`);
+for(let i=0;i<ele.length;i++){
+ele[i].href=`https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html`;
+}

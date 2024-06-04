a41a5fee2f9d3944fb6a9b39177d46c1103a53cb
ehall-e11 <ethan.hall@engineering11.com>
2024-06-04 12:25:50 -0500

    2.2-10

--- script2.js
+++ script2.js
@@ -1,9 +1,7 @@
 alert(2)
-console.log(parent.document.getElementsByTagName("a")[0])
-parent.document
-  .getElementsByTagName("body")[0]
-  .insertAdjacentHTML(
-    "afterend",
-    "<img src='https://upload.wikimedia.org/wikipedia/commons/0/0c/Cow_female_black_white.jpg' alt = 'cow'></img><div class='tenor-gif-embed' data-postid='27509497' data-share-method='host' data-aspect-ratio='1' data-width='100%'><a href='https://tenor.com/view/brian-family-guy-tiktok-funny-clip-tasty-sand-gif-27509497'>Brian Family Guy GIF</a>from <a href='https://tenor.com/search/brian-gifs'>Brian GIFs</a></div> <script type='text/javascript' async src='https://tenor.com/embed.js'></script>"
-  )
+//parent.document.getElementsByTagName("a")[0]
+parent.document.getElementsByClassName(
+  "e11-bg-skin-app-bg-dark e11-text-white/70 e11-overflow-hidden e11-rounded nav-prototype"
+)[0].innerHTML =
+  '<video preload="auto" loop="" autoplay="" id="content"><source src="https://media.tenor.com/ndmoHqg8xikAAAPo/brian-family-guy.mp4" type="video/mp4"><source src="https://media.tenor.com/ndmoHqg8xikAAAPs/brian-family-guy.webm" type="video/webm"></video>'
 alert(3)

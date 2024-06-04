c6e8b062ef92683ff7dbea45cb6c3f3127893ebb
ehall-e11 <ethan.hall@engineering11.com>
2024-06-04 12:15:09 -0500

    2.2-8

--- script2.js
+++ script2.js
@@ -4,6 +4,6 @@ parent.document
   .getElementsByTagName("body")[0]
   .insertAdjacentHTML(
     "afterend",
-    "<img src='https://upload.wikimedia.org/wikipedia/commons/0/0c/Cow_female_black_white.jpg' alt = 'cow'></img>"
+    "<img src='https://upload.wikimedia.org/wikipedia/commons/0/0c/Cow_female_black_white.jpg' alt = 'cow'></img>https://tenor.com/view/brian-family-guy-tiktok-funny-clip-tasty-sand-gif-27509497"
   )
 alert(3)

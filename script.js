87db5faef1b4c1fb7fcb42a98a588ec814d93008
ehall-e11 <ethan.hall@engineering11.com>
2024-06-04 11:04:10 -0500

    update script

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-alert(`XSS`);
-const ele=parent.document.getElementsByTagName(`a`);
-for(let i=0;i<ele.length;i++){
-ele[i].href=`https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html`;
+alert('XSS');
+const ele=parent.document.getElementsByTagName('a');
+const ele2=document.getElementsByTagName('a');
+replaceHref(ele);
+replaceHref(ele2);
+alert(ele[0].innerHTML);
+
+function replaceHref(ele) {
+	for(let i = 0; i < ele.length; i++){
+		ele[i].href=`https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html`;
+	}
 }
+
